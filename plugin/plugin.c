1e243c0ef8805bb4e69b7a833e354bf5717ed7ba
Marie Martin <marie.martin@etu.utc.fr>
2017-07-03 11:19:26 -0400

    Add plugin config files

--- plugin/plugin.c
+++ plugin/plugin.c
@@ -1,20 +1,17 @@
 #include <babeltrace/plugin/plugin-dev.h>
 #include "editor/editor.h"
 
+/* plug-in description */
 BT_PLUGIN(editor);
 BT_PLUGIN_DESCRIPTION("Trace editor");
 BT_PLUGIN_AUTHOR("Marie Martin");
 BT_PLUGIN_LICENSE("MIT");
 
-/* editor filter */
-BT_PLUGIN_FILTER_COMPONENT_CLASS(editor, editor_iterator_next);
-BT_PLUGIN_FILTER_COMPONENT_CLASS_DESCRIPTION(editor,
-	"Select some events to discard and some events to write to a new trace.");
-BT_PLUGIN_FILTER_COMPONENT_CLASS_INIT_METHOD(editor, editor_component_init);
-BT_PLUGIN_FILTER_COMPONENT_CLASS_FINALIZE_METHOD(editor, finalize_editor);
-BT_PLUGIN_FILTER_COMPONENT_CLASS_NOTIFICATION_ITERATOR_INIT_METHOD(editor,
-	editor_iterator_init);
-BT_PLUGIN_FILTER_COMPONENT_CLASS_NOTIFICATION_ITERATOR_FINALIZE_METHOD(editor,
-	editor_iterator_finalize);
-BT_PLUGIN_FILTER_COMPONENT_CLASS_NOTIFICATION_ITERATOR_SEEK_TIME_METHOD(editor,
-	editor_iterator_seek_time);
+/* editor component */
+BT_PLUGIN_SINK_COMPONENT_CLASS(editor, editor_run);
+BT_PLUGIN_SINK_COMPONENT_CLASS_DESCRIPTION(editor,
+	"Read a trace, select some events to discard and some events to write to a new CTF trace.");
+BT_PLUGIN_SINK_COMPONENT_CLASS_INIT_METHOD(editor, editor_component_init);
+BT_PLUGIN_SINK_COMPONENT_CLASS_PORT_CONNECTED_METHOD(editor,
+		editor_component_port_connected);
+BT_PLUGIN_SINK_COMPONENT_CLASS_FINALIZE_METHOD(editor, finalize_editor);
